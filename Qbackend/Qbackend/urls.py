7fdc1ec0cd0c4c2bb28756932834b285f9b3d67c
Dylan Smith <94149312+dsmith35@users.noreply.github.com>
2025-09-21 17:29:59 -0400

    Update urls.py

--- Qbackend/Qbackend/urls.py
+++ Qbackend/Qbackend/urls.py
@@ -1,24 +1,9 @@
-"""Qbackend URL Configuration
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/4.0/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
-
-# mysite/urls.py
+# Qbackend/urls.py
 from django.contrib import admin
-from django.urls import include, path
+from django.urls import include, path, re_path
 from rest_framework import routers
 from users import views
+from django.views.generic import TemplateView
 
 router = routers.DefaultRouter()
 router.register(r'scores', views.ScoresView, 'scores')
@@ -28,4 +13,7 @@ urlpatterns = [
     path('api/users/', include('users.urls')),
     path('api/', include(router.urls)),
     path('vs_bot/', include('vs_bot.urls')),
+
+    # Catch-all for React frontend
+    re_path(r'^.*$', TemplateView.as_view(template_name='index.html'), name='index'),
 ]
