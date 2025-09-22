c906972b45ce03af356846617b4db6be67257ba0
Dylan Smith <94149312+dsmith35@users.noreply.github.com>
2025-09-21 21:50:51 -0400

    Update startup.sh

--- startup.sh
+++ startup.sh
@@ -1,14 +1,19 @@
 #!/bin/bash
-set -e  # exit on first error
-echo "Building frontend..."
-cd frontend && npm install --legacy-peer-deps && npm run build
-cd ..
+set -e
+
+echo "PORT=$PORT"
+
+# Build frontend (optional, slow!)
+# cd frontend
+# npm install --legacy-peer-deps
+# npm run build
+# cd ..
+
 echo "Collecting static..."
 python Qbackend/manage.py collectstatic --noinput
+
 echo "Applying migrations..."
 python Qbackend/manage.py migrate
-echo "PORT=$PORT"
-echo "Starting Gunicorn..."
-exec gunicorn Qbackend.wsgi:application --bind=0.0.0.0:${PORT:-8000} --log-level=debug
 
-#
+echo "Starting Gunicorn..."
+exec gunicorn Qbackend.wsgi:application --bind=0.0.0.0:$PORT --log-level=debug
